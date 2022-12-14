107e7342a9610055451f5feb2939f8589d8ea833
Dimitrije Krstic <dimitrije.krstic@outlook.com>
2022-12-14 13:29:14 +0100

    new changes

---
+++ assets/Launcpad.b2c8a605.js
@@ -0,0 +1,1 @@
+import{P as e}from"./ProjectCard.65c5ca0b.js";import{a as c,o as _,c as o,l as a,_ as t}from"./index.38e7ac00.js";const s={class:"page page-launcpad"},n=c({__name:"Launcpad",setup(p){return(r,d)=>(_(),o("div",s,[a(e),a(e)]))}});const m=t(n,[["__scopeId","data-v-c1439391"]]);export{m as default};
---
+++ assets/languageDropdown.5a2b3dd5.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index.38e7ac00.js";const s=e({id:"languageDropdown",state:()=>({isLanguageDropdownOpen:!1,language:"English"}),actions:{changeStatus(){this.$state.isLanguageDropdownOpen=!this.$state.isLanguageDropdownOpen},closeDropdown(){this.$state.isLanguageDropdownOpen=!1},changeLanguage(a){this.$state.language=a}}});export{s as u};
